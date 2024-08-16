3af53ce13953eff7680774d29fae0f49404576a4
tarikaziz254 <tarikaziz2242@gmail.com>
2024-08-16 09:58:47 +0300

    creating models for library system

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length = 100)
 
 class Book(models.Model):
-    title = models.CharField(max_length = 255)
+    title = models.CharField(max_length = 100)
     author = models.ForeignKey(Author, on_delete = models.CASCADE, related_name = 'authors')
 
 class Library(models.Model):
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length = 100)
     books = models.ManyToManyField(Book)
 
 class Librarian(models.Model):
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length = 100)
     library = models.OneToOneField(Library, on_delete = models.CASCADE, related_name = 'library')
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,17 +2,17 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 #query all books by specific author
 author = Author.objects.create(name = 'Jane Austen')
-books_by_author = Book.objects.filter(author = author)
-print('Book by Jane Austen')
-for book in books_by_author:
+book_by_author = Book.objects.filter(author = author)
+for book in book_by_author:
     print(book.title)
 
+
 #list all books in library
-library = Library.objects.get(name = 'Nairobi')
-books_in_library = library.books.all()
+library = Library.objects.create(name = "Nairobi")
+books_in_library = Library.books.all()
 for book in books_in_library:
     print(book.title)
 
 #retrieve the librarian for a library
-librarian = Librarian.objects.get(library = library )
-print("librarian of Nairobi", librarian.name)
+librarian = Library.objects.get(library = 'Nairobi')
+print(librarian.name)
