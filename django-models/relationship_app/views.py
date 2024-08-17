008e9541b13c2d250085524c34b7ea928d92de8a
tarikaziz254 <tarikaziz2242@gmail.com>
2024-08-17 14:20:20 +0300

    fourth commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,6 +8,6 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
