96efd23adbd0d4d5f908c4435836364bb6cd0eec
tarikaziz254 <tarikaziz2242@gmail.com>
2024-08-17 14:07:52 +0300

    third commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,7 +4,7 @@ from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
