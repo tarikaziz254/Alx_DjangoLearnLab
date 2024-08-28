6b976c3536dbb93f5e7cb18a2530f8e870116abb
tarikaziz254 <tarikaziz2242@gmail.com>
2024-08-28 17:34:49 +0300

    created login, logout and signup files

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from . views import SignUpView
+from django.contrib.auth.views import LoginView as auth_views
+from django.contrib.auth.views import LogoutView as auth_views
+
 
 urlpatterns = [
     path('books/', list_books, name = 'list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name = 'library_detail'),
+    path('login/', auth_views.as_view(template_name = 'registratiion/login.html'), name = 'login'),
+    path('logout/', auth_views.as_view(template_name = 'registration/logout.html'), name = 'logout'),
+    path('signup/', SignUpView.as_view(), name = 'signup')
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
-
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
@@ -11,3 +15,8 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
