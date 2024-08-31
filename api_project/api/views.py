955a3c8049800bddfc92361847bdd4a7a98950f6
tarikaziz254 <tarikaziz2242@gmail.com>
2024-08-31 11:48:13 +0300

    removed a space between . and serializers and models in views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from . models import Book
-from . serializers import BookSerializer
+from .models import Book
+from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all
