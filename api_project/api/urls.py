07594b5ef8f4715f8e898366a90125d5428f3ea1
tarikaziz254 <tarikaziz2242@gmail.com>
2024-08-31 12:31:04 +0300

    added default router and BookList in url.py and views.py respectively

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . views import BookList
+from .views import BookList
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
 
+
+router = DefaultRouter()
+router.register(r'book-list', BookViewSet)
 urlpatterns = [
     path("book/", BookList.as_view(), name = "Book_list")
 ]
