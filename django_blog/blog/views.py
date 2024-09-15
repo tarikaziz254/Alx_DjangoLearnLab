7c45a2a7af3ed7a6a7b538671ed6341e4bfca792
tarikaziz254 <tarikaziz2242@gmail.com>
2024-09-15 15:07:55 +0300

    provided the autentication system for blog post

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in')
+            return redirect('login')
+        else:
+            form = UserRegisterForm()
+        return render(request, 'blog/register.html', {'form': form})
+    
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance = request.user )
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('profile')
+        
+    else:
+        form = UserUpdateForm(instance = request.user)
+    return render(request, 'blog/profile.html', {'form': form})
