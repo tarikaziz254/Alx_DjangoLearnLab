76d32f0eea697539d9a8b73176fbd6a5865a56c4
tarikaziz254 <tarikaziz2242@gmail.com>
2024-09-16 11:21:16 +0300

    changed comments to comment in the url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = 'post-delete'),
-    path('posts/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
